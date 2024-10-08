f183b08870d5b0a7bc1491b6f4bc04aabcaf2d79
enakshi31 <enakshi0148@gmail.com>
2024-10-08 11:59:10 +0000

    datatypes done

---
+++ basics/datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as newer version or newer js
+
+// alert( 3 + 3) // we are using node js not browser
+
+console.log(3 
+    + 3)  //code readability should be high so this is not the right way
+
+console.log("Enakshi")
+
+let name = "Enakshi"
+let age = 18
+let isLoggedIn = false
+
+//PRIMITIVE DATATYPES
+// number => 2 to power 53
+// bigInt if the number is very much big
+// string single quote or double quote => ""
+// boolean => true/false
+// null => standalone value (representation of an empty value)
+// undefined => when the value is not defined or assigned
+// we can also assign a null value then it will be defined but an empty value
+// symbol => unique
+
+
+// object
+
+console.log(typeof "enakshi"); //string
+console.log(typeof undefined); // output will be undefined
+console.log(typeof null); //type is object
