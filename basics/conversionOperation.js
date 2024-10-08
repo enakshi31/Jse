d1d049672723cbf1f11f80ce425c0410eb5382e2
enakshi31 <enakshi0148@gmail.com>
2024-10-08 13:03:08 +0000

    conversion

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -24,5 +24,46 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
  
 let stringNumber = string(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ****************** OPERATIONS ***********
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " enakshi"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); // output 12
+// console.log(1 + "2"); // output 12
+// console.log("1" + 2 + 2); // output 122
+// console.log(1 + 2 + "2"); // output 32
+
+// console.log( ( 3 + 4) * 5 % 3);
+
+// console.log(+true); //true+ is wrong //+true is also not preferable
+// console.log(+""); // not preferable
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
