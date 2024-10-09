7af1a7a566819c5a383604851823170e54638874
enakshi31 <enakshi0148@gmail.com>
2024-10-09 09:17:50 +0000

    stack and heap

--- basics/notesDatatypes.js
+++ basics/notesDatatypes.js
@@ -29,10 +29,40 @@ let myObj = {
 }
 
 const myFunction = function(){
-console.log("Hello world");
+// console.log("Hello world");
 }
 
-console.log(typeof bigNumber); // false, undefined
-console.log(typeof outsideTemp); //false, object
-console.log(typeof scoreValue); //false, number
-console.log(typeof myFunction); //false, object function
+// console.log(typeof bigNumber); // false, undefined
+// console.log(typeof outsideTemp); //false, object
+// console.log(typeof scoreValue); //false, number
+// console.log(typeof myFunction); //false, object function
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack memory (Primitive), Heap memory (Non primitive)
+
+let myYoutubename = "Enakshisingladotcom"
+
+let anothername = myYoutubename
+anothername = "Enaaaaaa"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "enakshi0148@gmail.com"
+    upi: "user@okaxis"
+}
+
+let userTwo = userOne
+
+userTwo.email = "enakshi@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+// stack mein copy milta h and heap mein reference milta h
+
